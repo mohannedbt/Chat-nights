c0172be9d83c648488366571b2398101c7c00274
mohannedbt <mohannedbentaleb8@gmail.com>
2025-06-25 16:01:37 +0100

    commented some console logs and revamped the design

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function generateRoomID() {
   for (let i = 0; i < 9; i++) {
     id += alphabet[Math.floor(Math.random() * (alphabet.length))];
   }
-  console.log('[ROOM ID GENERATED]', id);
+  // console.log('[ROOM ID GENERATED]', id);
   return id;
 }
 app.use('/sounds', express.static(__dirname + '/sounds'));
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 
 app.get('/submit', (req, res) => {
   const name = req.query.name;
-  console.log('[NAME SUBMIT]', 'Received name:', name);
+  // console.log('[NAME SUBMIT]', 'Received name:', name);
   if (!name) {
     return res.send("Please provide a valid name.");
   }
@@ -35,19 +35,19 @@ app.get('/submit', (req, res) => {
 });
 
 app.get('/menu.html', (req, res) => {
-  console.log('[ROUTE]', 'Serving menu.html');
+  // console.log('[ROUTE]', 'Serving menu.html');
   res.sendFile(__dirname + '/menu.html');
 });
 
 app.get('/chat.html', (req, res) => {
-  console.log('[ROUTE]', 'Serving chat.html');
+  // console.log('[ROUTE]', 'Serving chat.html');
   res.sendFile(__dirname + '/chat.html');
 });
 
 app.get('/join', (req, res) => {
   const name = req.query.name || "Anonymous";
   const joinroomid = req.query.roomCode || "none";
-  console.log('[JOIN REQUEST]', `Name: ${name} | Room Code: ${joinroomid}`);
+  // console.log('[JOIN REQUEST]', `Name: ${name} | Room Code: ${joinroomid}`);
   res.redirect(`/chat.html?name=${encodeURIComponent(name)}&roomid=${encodeURIComponent(joinroomid)}`);
 });
 
@@ -57,20 +57,20 @@ io.on('connection', (socket) => {
   let current = "anonymous"; // Default name
   let socketRoomID = null;
 
-  console.log('[SOCKET CONNECTED]', `Socket ID: ${socket.id} | Total users: ${user}`);
+  // console.log('[SOCKET CONNECTED]', `Socket ID: ${socket.id} | Total users: ${user}`);
 
   socket.on('name el personne', (name) => {
     current = name;
-    console.log('[USER SET NAME]', `Socket: ${socket.id} | Name set to: ${current}`);
+    // console.log('[USER SET NAME]', `Socket: ${socket.id} | Name set to: ${current}`);
 
     if (socketRoomID) {
-      console.log('[USER RECONNECTED TO ROOM]', `${current} in room ${socketRoomID}`);
+      // console.log('[USER RECONNECTED TO ROOM]', `${current} in room ${socketRoomID}`);
       io.to(socketRoomID).emit('connecta', `${current} has connected`);
     }
   });
 
   socket.on('message men 3andi', (message) => {
-    console.log('[MESSAGE]', `From: ${current} | Room: ${socketRoomID} | Message: ${message}`);
+    // console.log('[MESSAGE]', `From: ${current} | Room: ${socketRoomID} | Message: ${message}`);
     if (socketRoomID) {
       io.to(socketRoomID).emit('message men 3andi', message);
     }
@@ -86,22 +86,22 @@ io.on('connection', (socket) => {
     socket.join(newRoomID);
     socketRoomID = newRoomID;
 
-    console.log('[ROOM CREATED]', `Room ID: ${newRoomID} | Created by: ${current}`);
+    // console.log('[ROOM CREATED]', `Room ID: ${newRoomID} | Created by: ${current}`);
 
     callback(newRoomID);
   });
 
   socket.on('join room', (joinRoomID) => {
-    console.log('[JOIN ROOM]', `${current} is attempting to join room: ${joinRoomID}`);
+    // console.log('[JOIN ROOM]', `${current} is attempting to join room: ${joinRoomID}`);
 
     if (current === "anonymous") {
-      console.log('[ERROR]', 'Anonymous user cannot join room.');
+      // console.log('[ERROR]', 'Anonymous user cannot join room.');
       io.to(socket.id).emit("error", "Please set your name before joining the room.");
       return;
     }
 
     if (!room[joinRoomID]) {
-      console.log('[ROOM NOT FOUND]', `Creating new room: ${joinRoomID}`);
+      // console.log('[ROOM NOT FOUND]', `Creating new room: ${joinRoomID}`);
       room[joinRoomID] = { id: joinRoomID, users: [] };
     }
 
@@ -109,14 +109,14 @@ io.on('connection', (socket) => {
     socketRoomID = joinRoomID;
     room[joinRoomID].users.push(socket.id);
 
-    console.log('[USER JOINED ROOM]', `${current} joined room ${joinRoomID}`);
+    // console.log('[USER JOINED ROOM]', `${current} joined room ${joinRoomID}`);
    socket.emit("room info","votre room code est "+joinRoomID);
     io.to(socketRoomID).emit('message men 3andi', `${current} has joined the room.`);
   });
 
   socket.on('disconnect', () => {
     user--;
-    console.log('[DISCONNECT]', `${current} disconnected | Socket: ${socket.id} | Remaining users: ${user}`);
+    // console.log('[DISCONNECT]', `${current} disconnected | Socket: ${socket.id} | Remaining users: ${user}`);
 
     if (socketRoomID) {
       io.to(socketRoomID).emit("message men 3andi", `${current} has disconnected. Number of users staying is: ${user}`);
@@ -127,6 +127,6 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`[SERVER STARTED] Listening on http://0.0.0.0:${PORT}`);
+  // console.log(`[SERVER STARTED] Listening on http://0.0.0.0:${PORT}`);
 });
 
